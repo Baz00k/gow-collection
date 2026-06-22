d4cc7fde14e815edf9637891ba95908be0b71d00
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2026-06-22 16:18:02 +0200

    chore: remove obsolete startup scripts execution

--- images/prism-offline/build/scripts/startup.sh
+++ images/prism-offline/build/scripts/startup.sh
@@ -4,14 +4,6 @@ source /opt/gow/bash-lib/utils.sh
 
 PrismLauncher=prismlauncher
 
-# Run additional startup scripts from /opt/gow/startup.d/
-for file in /opt/gow/startup.d/* ; do
-    if [ -f "$file" ] ; then
-        gow_log "[start] Sourcing $file"
-        source "$file"
-    fi
-done
-
 gow_log "[start] Starting PrismLauncher"
 
 source /opt/gow/launch-comp.sh
